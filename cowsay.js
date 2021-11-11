0d2bab64dc9581d8722ddcd3c082ab464ea8c989
kasparkretz <kaspar.wim.kretz@gmail.com>
2021-11-11 15:46:45 +0100

    Randomize number of cows

--- cowsay.js
+++ cowsay.js
@@ -2,29 +2,23 @@ const express = require("express");
 const cowsay = require("cowsay");
 const app = express();
 
-function get_cows(error, cow_names) {
-  if (error) {
-      console.log(error)
+function makeCows(count) {
+  var cows = "";
+  for (i=0; i<count; i++) {
+    cows += cowsay.say({
+      text: "I'm random cow number " + (i + 1) ,
+      e: "**",
+      T: "U",
+      r: true
+      }) + "\n";
   }
-  else if (cow_names) {
-      console.log(`Number of cows available: ${cow_names.length}`);
-      for (i=0;i<cow_names.length;i++) {
-        console.log(cow_names[i]);
-      }
-  }
+  return cows;
 }
 
 app.get("/", function (req, res) {
-  cowsay.list(get_cows);
+  count = Math.floor((Math.random() * 1000) + 1);
   res.send(
-    "<pre>" +
-      cowsay.say({
-        text: "please reload the site!j",
-        e: "**",
-        T: "U",
-        r:true
-        }) +
-      "</pre>"
+    "<pre>" + makeCows(count) + "</pre>"
   );
 });
 
