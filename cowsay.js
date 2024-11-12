f4135151ad242b273f462bb3aaccb573874c2b30
Mathis Kretz <mkretz@users.noreply.github.com>
2024-11-12 07:37:58 +0100

    Revert "Randomize number of cows"
    This reverts commit 0d2bab64dc9581d8722ddcd3c082ab464ea8c989.

--- cowsay.js
+++ cowsay.js
@@ -2,23 +2,29 @@ const express = require("express");
 const cowsay = require("cowsay");
 const app = express();
 
-function makeCows(count) {
-  var cows = "";
-  for (i=0; i<count; i++) {
-    cows += cowsay.say({
-      text: "I'm random cow number " + (i + 1) ,
-      e: "**",
-      T: "U",
-      r: true
-      }) + "\n";
+function get_cows(error, cow_names) {
+  if (error) {
+      console.log(error)
   }
-  return cows;
+  else if (cow_names) {
+      console.log(`Number of cows available: ${cow_names.length}`);
+      for (i=0;i<cow_names.length;i++) {
+        console.log(cow_names[i]);
+      }
+  }
 }
 
 app.get("/", function (req, res) {
-  count = Math.floor((Math.random() * 1000) + 1);
+  cowsay.list(get_cows);
   res.send(
-    "<pre>" + makeCows(count) + "</pre>"
+    "<pre>" +
+      cowsay.say({
+        text: "please reload the site!j",
+        e: "**",
+        T: "U",
+        r:true
+        }) +
+      "</pre>"
   );
 });
 
