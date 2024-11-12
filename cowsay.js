0748ae40abdb6eabc85e19901e401164e9880825
Mathis Kretz <mkretz@users.noreply.github.com>
2024-11-12 07:38:01 +0100

    Revert "Change text, randomize cows"
    This reverts commit ccc910767adf903d788f5ce0a88e5a387955fe9e.

--- cowsay.js
+++ cowsay.js
@@ -2,28 +2,14 @@ const express = require("express");
 const cowsay = require("cowsay");
 const app = express();
 
-function get_cows(error, cow_names) {
-  if (error) {
-      console.log(error)
-  }
-  else if (cow_names) {
-      console.log(`Number of cows available: ${cow_names.length}`);
-      for (i=0;i<cow_names.length;i++) {
-        console.log(cow_names[i]);
-      }
-  }
-}
-
 app.get("/", function (req, res) {
-  cowsay.list(get_cows);
   res.send(
     "<pre>" +
       cowsay.say({
-        text: "please reload the site!",
-        e: "**",
-        T: "U",
-        r:true
-        }) +
+        text: "Mooooh from the cloud!",
+        e: "oO",
+        T: "U ",
+      }) +
       "</pre>"
   );
 });
